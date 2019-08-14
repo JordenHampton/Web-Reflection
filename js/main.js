a82f5b79cdfa2bc6e0801be605a9ca56125b74ba
JordenHampton <platinumgamer98@gmail.com>
2019-08-14 12:46:08 +0100

    Added javascript functionality
    One bug remaining, main nav dropdown is squished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const fig = document.querySelector('.fig');
+const cookieClose = document.querySelector('.cookie-close');
+const pageCover = document.querySelector('.page-cover');
 let active = false;
-document.cookie = "cookie=true"; 
 
 fig.addEventListener('click', () => {
 
@@ -22,15 +23,25 @@ fig.addEventListener('click', () => {
         document.querySelector('.line-1').style.transform = "rotate(45deg)";
         document.querySelector('.line-3').style.transform = "rotate(-45deg)";
 
+        // Move page to the side
+
         document.querySelector('.mobile-nav-container').style.display = "initial";
         document.querySelector('.main-container').style.marginLeft = "-18rem";
         document.querySelector('header').style.marginLeft = "-18rem";
         document.querySelector('.nav-sep').style.width = "35vw";
 
+        // Stop page from scrolling
+        document.querySelector('body').style.overflowY = "hidden";
+
+        // Show a transparent layer over content when menu is open
+        document.querySelector('.page-cover').style.display = "initial";
+
+
+        // To keep track of if menu is opened or closed.
         active = true;
 
     } else {
-        // Merge lines and hide 1
+        // Unmerge lines and show 1
         document.querySelector('.line-2').style.margin = ".3rem 0";
         document.querySelector('.line-2').style.visibility = "visible";
 
@@ -43,7 +54,7 @@ fig.addEventListener('click', () => {
         document.querySelector('.line-3').style.width = "2rem";
 
 
-        // Rotate lines into a cross
+        // Rotate lines into a straight line
         
         document.querySelector('.line-1').style.transform = "rotate(0deg)";
         document.querySelector('.line-3').style.transform = "rotate(0deg)";
@@ -52,7 +63,50 @@ fig.addEventListener('click', () => {
         document.querySelector('.main-container').style.marginLeft = "0rem";
         document.querySelector('header').style.marginLeft = "0rem";
 
+
+        // Allow page scrolling
+        document.querySelector('body').style.overflowY = "initial";
+
+        // Hide transparent layer
+        document.querySelector('.page-cover').style.display = "none";
+
         active = false;
     }
 });
+
+pageCover.addEventListener('click', () => {
+    
+        // Unmerge lines and show 1
+        document.querySelector('.line-2').style.margin = ".3rem 0";
+        document.querySelector('.line-2').style.visibility = "visible";
+
+        // Make space for objects
+        document.querySelector('.burger-container').style.marginTop = ".34rem";
+        document.querySelector('.burger-container').style.height = "1.8rem";
+
+        // Reduce width of lines to fit better
+        document.querySelector('.line-1').style.width = "2rem";
+        document.querySelector('.line-3').style.width = "2rem";
+
+
+        // Rotate lines into a straight line
+        
+        document.querySelector('.line-1').style.transform = "rotate(0deg)";
+        document.querySelector('.line-3').style.transform = "rotate(0deg)";
+
+        document.querySelector('.mobile-nav-container').style.display = "none";
+        document.querySelector('.main-container').style.marginLeft = "0rem";
+        document.querySelector('header').style.marginLeft = "0rem";
+
+
+        // Allow page scrolling
+        document.querySelector('body').style.overflowY = "initial";
+
+        // Hide transparent layer
+        document.querySelector('.page-cover').style.display = "none";
+
+        active = false;
+    
+});
+
 
